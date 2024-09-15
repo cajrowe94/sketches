caa15aa47325c235873b0937f5510e71a2505fff
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2024-09-15 10:38:07 -0400

    werk

--- clouds/index.js
+++ clouds/index.js
@@ -5,7 +5,7 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    new Cloud(300, 600, 500, 500, 5, 0.1).render();
+    new Cloud(300, 1300, 500, 500, 10, 0.2).render();
 }
 
 class Cloud {
@@ -15,9 +15,7 @@ class Cloud {
     #yEnd;
     #pivotSteps;
     #pivotExtreme;
-    #cloudMidPoint;
-    #cloudPeakTop;
-    #cloudPeakBottom;
+    #pivotPointCoords;
 
     constructor(xStart, xEnd, yStart, yEnd, pivotSteps, pivotExtreme) {
         /**
@@ -31,35 +29,38 @@ class Cloud {
         this.#yEnd = yEnd;
         this.#pivotSteps = pivotSteps;
         this.#pivotExtreme = pivotExtreme > 1 ? 1 : pivotExtreme;
-        this.#cloudMidPoint = Math.floor((xStart + xEnd) / 2);
-        this.#cloudPeakTop = random(10, 200);
-        this.#cloudPeakBottom = random(10, 75);
+        this.#pivotPointCoords = [];
     }
 
     render() {
-        this.drawCloudFrame();
-        // this.drawBaseLayer();
+        this.buildCloudFrame(false);
+        this.drawBaseLayer();
     }
 
     drawBaseLayer() {
-        for (let i = this.#xStart; i < this.#xEnd; i += 10) {
-            const arcX = i;
-            const arcY = this.#yStart;
-            const arcBulk = random(10, 20);
+        this.#pivotPointCoords.forEach(point => {
+            // noFill();
 
-            arc(arcX, arcY, arcBulk, arcBulk, PI, 0);
-        }
+            // chunk
+            let chunkSpread = [10, 50];
+            arc(point[0], point[1], 50, 50, random(1, PI), TWO_PI);
+            arc(point[0] - random(...chunkSpread), point[1] - random(...chunkSpread), 50, 50, random(1, PI), TWO_PI);
+            arc(point[0] + random(...chunkSpread), point[1] + random(...chunkSpread), 50, 50, random(1, PI), TWO_PI);
+
+            // sprinkles
+
+        });
     }
 
-    drawCloudFrame() {
+    buildCloudFrame(showFrame = false) {
         const noiseLevel = 1000 * this.#pivotExtreme;
         const noiseScale = 0.02;
         const cloudWidth = this.#xEnd - this.#xStart;
 
-        const drawCloudEdge = (dir) => {
+        const drawCloudEdge = dir => {
             let nextX = this.#xStart;
             let nextY = this.#yStart;
-            let xStartLoop = round(this.#xStart + (cloudWidth * random(0.1, 0.4)));
+            let xStartLoop = round(this.#xStart + (cloudWidth * random(0, 0.2)));
             const workingWidth = round(this.#xEnd - xStartLoop);
 
             for (let i = xStartLoop; i < this.#xEnd; i += (round(workingWidth / this.#pivotSteps))) {
@@ -72,23 +73,31 @@ class Cloud {
                 } else {
                     y = this.#yStart + noiseLevel * noise(nx)
                 }
-    
-                circle(nextX, nextY, 10);
-                line(nextX, nextY, x, y);
+                
+                if (showFrame) {
+                    circle(nextX, nextY, 10);
+                    line(nextX, nextY, x, y);
+                }
+
                 
                 nextX = i;
                 nextY = y;
+                this.#pivotPointCoords.push([nextX, nextY]);
             }
-    
-            circle(nextX, nextY, 10);
-            line(nextX, nextY, this.#xEnd, this.#yEnd);
+            
+            if (showFrame) {
+                circle(nextX, nextY, 10);
+                line(nextX, nextY, this.#xEnd, this.#yEnd);
+            }
         }
 
         drawCloudEdge('top');
-        drawCloudEdge('bottom');
+        drawCloudEdge();
 
-        // cloud base line
-        line(this.#xStart, this.#yStart, this.#xEnd, this.#yEnd);
+        // cloud center base line
+        if (showFrame) {
+            line(this.#xStart, this.#yStart, this.#xEnd, this.#yEnd);
+        }
     }
 }
 
